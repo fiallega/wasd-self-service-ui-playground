357382b9e536f8f35d8642ed137b69fea3527c31
Jorge Fiallega <jorgefiallega@gmail.com>
2019-02-04 16:14:25 -0500

    add account manager component to accounts module: ng generate component accounts/account-manager

--- src/app/accounts/accounts.module.ts
+++ src/app/accounts/accounts.module.ts
@@ -1,8 +1,10 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { AccountManagerComponent } from './account-manager/account-manager.component';
 
-@NgModule({
-  declarations: [],
+
+  @NgModule({
+  declarations: [AccountManagerComponent],
   imports: [
     CommonModule
   ]
